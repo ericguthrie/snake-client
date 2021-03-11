4f4ba9dd7189e1453a5e96626c9a16d9ace130d8
ericguthrie <ericguthrie12@gmail.com>
2021-03-11 03:22:38 +0000

    final snake

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+const { IP, PORT } = require('./constants');
 const net = require('net');
 
 /**
@@ -5,12 +6,9 @@ const net = require('net');
  */
 const connect = function() {
 
-
-
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-    
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
@@ -26,13 +24,7 @@ const connect = function() {
       conn.write('Name: GUS');
     });
 
-    const setupInput = function() {
-      const stdin = process.stdin;
-      stdin.setRawMode(true);
-      stdin.setEncoding('utf8');
-      stdin.resume();
-      return stdin;
-    }
+
 
 
 
