56b7a94598f089206d37a0692c1a5b0c3f7f89d1
ericguthrie <ericguthrie12@gmail.com>
2021-03-11 01:12:17 +0000

    snake moving

--- client.js
+++ client.js
@@ -26,6 +26,62 @@ const connect = function() {
       conn.write('Name: GUS');
     });
 
+//     conn.on('connect', () => {
+//       conn.write('Move: down');
+
+// });
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 200);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 400);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 600);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 800);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 1000);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 1200);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 1400);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 1600);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 1800);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 2000);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 2200);
+
+// setTimeout(() => {
+//   conn.write('Move: right');
+// }, 2400);
+
+
+
+
   return conn;
 
   
